141f24ebc8391b6dd276ba9275760fdfb0bcd80f
Ashish <negiashish300@gmail.com>
2024-07-29 16:26:18 +0530

    changes made to setup and requirement file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='Ashish Negi',
+    author_email='ashish10314@gmail.com',
+    packages=find_packages()
+)
